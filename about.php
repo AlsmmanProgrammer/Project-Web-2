c3a579ed099f3b2c4d94f15e20689a5997aaf6cc
basheer <basheersamman2003@gmail.com>
2025-10-17 20:36:35 +0300

    edit design of about page

--- about.php
+++ about.php
@@ -52,26 +52,63 @@ $lang_link = $current_path . '?' . http_build_query($query);
 
     <section class="py-5 bg-light">
         <div class="container">
-            <h3 class="text-center fw-bold mb-5" data-aos="fade-up"><?php echo htmlspecialchars($lang['about_team']); ?></h3>
-            <div class="row gy-4">
+            <h3 class="text-center fw-bold mb-5" data-aos="fade-up">
+                <?php echo htmlspecialchars($lang['about_team']); ?>
+            </h3>
+
+            <div class="row justify-content-center gy-4">
                 <?php
                 $team = [
-                    ["name" => "أحمد السمان", "role" => "المدير التنفيذي", "img" => "team-1.jpg"],
-                    ["name" => "ليلى مراد", "role" => "مديرة الفعاليات", "img" => "team-2.jpg"],
-                    ["name" => "محمد الخطيب", "role" => "مصمم واجهات", "img" => "team-3.jpg"],
+                    [
+                        "name_ar" => "بشير السمان",
+                        "name_en" => "Basheer Alsamman",
+                        "role_en" => "Full Stack Developer",
+                        "img" => "https://cdn-icons-png.flaticon.com/512/921/921071.png"
+                    ],
+                    [
+                        "name_ar" => "ملاك جيرون",
+                        "name_en" => "Malak Jiroun",
+                        "role_en" => "Front-End Developer",
+                        "img" => "https://cdn-icons-png.flaticon.com/512/4140/4140047.png"
+                    ],
+                    [
+                        "name_ar" => "آية عبد المولى",
+                        "name_en" => "Aya Abdulmawla",
+                        "role_en" => "Front-End Developer",
+                        "img" => "https://cdn-icons-png.flaticon.com/512/4140/4140051.png"
+                    ],
+                    [
+                        "name_ar" => "حلا خالد",
+                        "name_en" => "Hala Khaled",
+                        "role_en" => "Front-End Developer",
+                        "img" => "https://cdn-icons-png.flaticon.com/512/4140/4140047.png"
+                    ],
+                    [
+                        "name_ar" => "راما الحلاق",
+                        "name_en" => "Rama Alhallak",
+                        "role_en" => "Front-End Developer",
+                        "img" => "https://cdn-icons-png.flaticon.com/512/4140/4140051.png"
+                    ],
                 ];
-                foreach ($team as $member): ?>
-                    <div class="col-md-4" data-aos="fade-up" data-aos-delay="100">
-                        <div class="card team-card text-center border-0 shadow-sm">
-                            <img src="assets/img/team/<?php echo htmlspecialchars($member['img']); ?>" class="card-img-top" alt="Team" style="height:250px; object-fit:cover;">
+
+                foreach ($team as $index => $member): ?>
+                    <div class="col-sm-6 col-md-4 col-lg-4" data-aos="fade-up" data-aos-delay="<?php echo 100 * $index; ?>">
+                        <div class="card team-card border-0 shadow-sm rounded-4 h-100 text-center">
+                            <div class="p-3">
+                                <img src="<?php echo htmlspecialchars($member['img']); ?>"
+                                    class="rounded-circle shadow-sm"
+                                    alt="<?php echo htmlspecialchars($member['name_ar']); ?>"
+                                    style="width:140px; height:140px; object-fit:cover;">
+                            </div>
                             <div class="card-body">
-                                <h5 class="card-title mb-1"><?php echo htmlspecialchars($member['name']); ?></h5>
-                                <p class="text-muted small mb-2"><?php echo htmlspecialchars($member['role']); ?></p>
-                                <div>
-                                    <a href="#" class="text-secondary mx-1"><i class="fab fa-facebook"></i></a>
-                                    <a href="#" class="text-secondary mx-1"><i class="fab fa-linkedin"></i></a>
-                                    <a href="#" class="text-secondary mx-1"><i class="fab fa-instagram"></i></a>
-                                </div>
+                                <h5 class="fw-bold mb-1">
+                                    <?php echo htmlspecialchars($member['name_en']); ?><br>
+                                    <small class="text-muted"><?php echo htmlspecialchars($member['name_ar']); ?></small>
+                                </h5>
+                                <p class="text-secondary mb-2">
+                                    <?php echo htmlspecialchars($member['role_en']); ?><br>
+                                </p>
+                                
                             </div>
                         </div>
                     </div>
